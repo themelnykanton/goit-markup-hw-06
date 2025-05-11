6b4f551af763e2a2ebbc96d4aa452d5e64638179
themelnykanton <themelnykanton@gmail.com>
2025-05-11 20:33:02 +0200

    everything-works

--- js/script.js
+++ js/script.js
@@ -1,32 +1,49 @@
-<script>
-  document.addEventListener('DOMContentLoaded', () => {
-    const openModalBtn = document.querySelector('.main-section-button');
-    const modalOverlay = document.querySelector('.modal-overlay');
-    const closeModalBtn = document.querySelector('.modal-close-button');
+document.addEventListener('DOMContentLoaded', () => {
+  const openModalBtn = document.querySelector('.main-section-button');
+  const modalOverlay = document.querySelector('.modal-overlay');
+  const closeModalBtn = document.querySelector('.modal-close-button');
 
-    // Открыть модалку
-    openModalBtn.addEventListener('click', () => {
-      modalOverlay.classList.add('is-visible');
-    });
-
-    // Закрыть по кнопке
-    closeModalBtn.addEventListener('click', () => {
-      modalOverlay.classList.remove('is-visible');
-    });
-
-    // Закрыть по клику вне модального окна
-    modalOverlay.addEventListener('click', (event) => {
-      // Проверяем, что клик был именно по оверлею, а не по содержимому
-      if (event.target === modalOverlay) {
-        modalOverlay.classList.remove('is-visible');
-      }
-    });
-
-    // Закрыть по Esc
-    window.addEventListener('keydown', (event) => {
-      if (event.key === 'Escape') {
-        modalOverlay.classList.remove('is-visible');
-      }
-    });
+  openModalBtn.addEventListener('click', () => {
+    modalOverlay.classList.add('is-visible');
   });
-</script>
+
+  closeModalBtn.addEventListener('click', () => {
+    modalOverlay.classList.remove('is-visible');
+  });
+
+  modalOverlay.addEventListener('click', (event) => {
+    if (event.target === modalOverlay) {
+      modalOverlay.classList.remove('is-visible');
+    }
+  });
+
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      modalOverlay.classList.remove('is-visible');
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const burgerOpenButton = document.querySelector(".burger-open-button");
+  const burgerMenuOverlay = document.querySelector(".burger-menu-overlay");
+  const modalCloseButton = document.querySelector(".burger-close-button");
+
+  burgerOpenButton.addEventListener("click", () => {
+    burgerMenuOverlay.classList.add("is-visible");
+    document.body.classList.add("body-no-scroll");
+  });
+
+  modalCloseButton.addEventListener("click", () => {
+    burgerMenuOverlay.classList.remove("is-visible");
+    document.body.classList.remove("body-no-scroll");
+  });
+
+  burgerMenuOverlay.addEventListener("click", (event) => {
+    if (event.target === burgerMenuOverlay) {
+      burgerMenuOverlay.classList.remove("is-visible");
+      document.body.classList.remove("body-no-scroll");
+    }
+  });
+});
+
